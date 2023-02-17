88cb1c5ea63545def19afc8abca87b1e0028d324
developer <ddd98754@gmail.com>
2023-02-18 00:02:37 +0900

    ログイン状態時のみ掲示板表示するよう変更

--- board/views.py
+++ board/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
-class BoardView(TemplateView):
+class BoardView(LoginRequiredMixin, TemplateView):
     template_name = 'board/board.html'
