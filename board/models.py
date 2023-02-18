d8eb1bd706d806d1f2f9c534cdcd7afcb2730282
developer <ddd98754@gmail.com>
2023-02-18 12:50:38 +0900

    warning修正のためPostモデルのpub_dateのデフォルト変更

--- board/models.py
+++ board/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from accounts.models import Employee
-from django.utils import timezone
+from django.utils.timezone import now
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
     contents = models.CharField(max_length=800)
-    pub_date = models.DateTimeField(default=timezone.now())
+    pub_date = models.DateTimeField(default=now)
 
     contributer = models.CharField(max_length=150)
 
