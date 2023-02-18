0f434bec92b7d7d9d5bd3b0bdc2f1aceb4e1393d
developer <ddd98754@gmail.com>
2023-02-18 12:09:44 +0900

    ログイン中、ログインページにアクセスした際にトップページにリダイレクトするよう変更

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
 
 class CustomLoginView(LoginView):
+    redirect_authenticated_user = True
     template_name = 'accounts/login.html'
 
 class CustomLogoutView(LogoutView):
